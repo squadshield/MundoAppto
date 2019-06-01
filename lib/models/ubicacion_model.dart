00a6278396042167fcfd3347331b271ef1ee8836
DBAOscar <adrian_ortiz92>
2019-06-01 10:45:51 -0500

    Listado de mapa V3

---
+++ lib/models/ubicacion_model.dart
@@ -0,0 +1,127 @@
+class Ubicaciones{
+  List<Ubicacion> items =  new List();
+
+  Ubicaciones();
+  Ubicaciones.fromJsonList(List<dynamic> jsonList){
+    if(jsonList =null)return;
+    for (var item in jsonList) {
+      final ubicacion = new Ubicacion.fromJsonMap(item);
+      items.add(ubicacion);   
+    }
+  }
+}
+class Ubicacion {
+  String formattedAddress;
+  Geometry geometry;
+  String icon;
+  String id;
+  String name;
+  OpeningHours openingHours;
+  List<Photo> photos;
+  String placeId;
+  PlusCode plusCode;
+  int priceLevel;
+  double rating;
+  String reference;
+  List<Type> types;
+  int userRatingsTotal;
+
+  Ubicacion({
+    this.formattedAddress,
+    this.geometry,
+    this.icon,
+    this.id,
+    this.name,
+    this.openingHours,
+    this.photos,
+    this.placeId,
+    this.plusCode,
+    this.priceLevel,
+    this.rating,
+    this.reference,
+    this.types,
+    this.userRatingsTotal 
+  });
+
+  Ubicacion.fromJsonMap(Map<String,dynamic> json){
+      formattedAddress =  json["formatted_address"];
+      geometry         =   json["geometry"];
+      icon          = json["icon"];
+      id            =json["id"];
+      name          = json["name"];
+      openingHours  = json["opening_Hours"];
+      photos        = json["photos"];
+      placeId       = json["place_Id"];
+      plusCode      = json["plus_Code"];
+      priceLevel    = json["price_Level"];
+      rating        = json["rating"];
+      reference     = json["reference"];
+      types         = json["types"];
+      userRatingsTotal = json["user_Ratings_Total"];
+  }
+}
+
+
+class Geometry {
+  Location location;
+  Viewport viewport;
+
+  Geometry({
+    this.location,
+    this.viewport,
+  });
+}
+
+class Location {
+  double lat;
+  double lng;
+
+  Location({
+    this.lat,
+    this.lng,
+  });
+}
+
+class Viewport {
+  Location northeast;
+  Location southwest;
+
+  Viewport({
+    this.northeast,
+    this.southwest,
+  });
+}
+
+class OpeningHours {
+  bool openNow;
+
+  OpeningHours({
+    this.openNow,
+  });
+}
+
+class Photo {
+  int height;
+  List<String> htmlAttributions;
+  String photoReference;
+  int width;
+
+  Photo({
+    this.height,
+    this.htmlAttributions,
+    this.photoReference,
+    this.width,
+  });
+}
+
+class PlusCode {
+  String compoundCode;
+  String globalCode;
+
+  PlusCode({
+    this.compoundCode,
+    this.globalCode,
+  });
+}
+
+enum Type { RESTAURANT, FOOD, POINT_OF_INTEREST, ESTABLISHMENT, BAR, CAFE, NIGHT_CLUB }
--- lib/providers/listado_provider.dart
+++ lib/providers/listado_provider.dart
@@ -1,7 +1,23 @@
-class ListadoProvider {
-List<Map<String, dynamic>> users = [
-  { "nombre": "UTEC", "descripcion": "Universidad de Ingeniería y Tecnología", "ubicacion":"Jr Calle 123", "ranking":"5.0", "imagen":"https://www.utec.edu.pe/sites/default/files//styles/noticias_slider/public/noticias/web_grande_1.png", "comentario":"Buen lugar para ser el mejor", "lat" : -12.15555617010728, "lng" : -77.02123532010726 },
-  { "nombre": "Estación Aramburú", "descripcion": "Estación del Metropolitano", "ubicacion":"Av. La nueva Esperanza 201", "ranking":"3.9", "imagen":"https://fastly.4sqi.net/img/general/width960/43mI9lDWgx9P51A3QNx0MQMn1MUjNkPsY3NEAqZGB6M.jpg", "comentario":"Mucha cola para solo un tramo corto. Deberían haber mas expresos!", "lat" : -12.15144617010728, "lng" : -77.02123082010726  },
-  { "nombre": "Restaurante Marita", "descripcion": "Buena sazón!", "ubicacion":"Av. Los Robles 441", "ranking":"4.1", "imagen":"https://u.tfstatic.com/restaurant_photos/029/411029/169/612/masterchef-vista-sala-188d9.jpg", "comentario":"La señora atiende muy bien","lat" : -12.15234617010728, "lng" : -77.02333082010726  },
-];
+import 'dart:convert';
+import '../models/ubicacion_model.dart';
+//https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+PERU&key=AIzaSyBuKra5jDVBSEFRi1tlZ_Cww16AXebA0pc
+
+import 'package:http/http.dart' as http;
+class UbicacionProvider {
+  String _url ="maps.googleapis.com";
+  String _query="restaurants+in+PERU";
+  String _key = "AIzaSyBuKra5jDVBSEFRi1tlZ_Cww16AXebA0pc";
+
+  Future<List<Ubicacion>> getUbicaciones() async{
+    final url = Uri.https(_url, "/maps/api/place/textsearch/json",{
+      'query':_query,
+      'key':_key
+    });
+    
+      final resp = await http.get(url);
+
+      final decodeData = json.decode(resp.body);
+      print(decodeData);
+      return [];
+  }
 }
