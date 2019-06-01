8673d9058b071558ba503fa2ade667605c50f961
DBAOscar <adrian_ortiz92>
2019-06-01 11:53:50 -0500

    Corrección issue de contenido.

--- lib/providers/listado_provider.dart
+++ lib/providers/listado_provider.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 import '../models/ubicacion_model.dart';
 //https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+PERU&key=AIzaSyBuKra5jDVBSEFRi1tlZ_Cww16AXebA0pc
-
 import 'package:http/http.dart' as http;
+
 class UbicacionProvider {
   String _url ="maps.googleapis.com";
   String _query="restaurants+in+PERU";
   String _key = "AIzaSyBuKra5jDVBSEFRi1tlZ_Cww16AXebA0pc";
+
+  //List<Ubicacion> lista = [{""}]
 
   Future<List<Ubicacion>> getUbicaciones() async{
     final url = Uri.https(_url, "/maps/api/place/textsearch/json",{
@@ -18,6 +20,8 @@ class UbicacionProvider {
       print(resp);
       final decodeData = json.decode(resp.body);
       final ubicaciones = new Ubicaciones.fromJsonList(decodeData["results"]);
+
+      
       return ubicaciones.items;
   }
 }
