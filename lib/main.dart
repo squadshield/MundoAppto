df0ed287dad6450ed97243b3c336501e418ebed8
Piero Bravo <pbravogutarra@outlook.com>
2019-06-01 12:02:16 -0500

    merge main dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,6 @@ import 'package:flutter/material.dart';
 // import 'package:mundoappto/pages/home.dart';
 import 'package:mundoappto/pages/pruebaFirebase.dart';
 
-<<<<<<< HEAD
-// import 'card_page_dart.dart';
-
-=======
->>>>>>> 8673d9058b071558ba503fa2ade667605c50f961
 void main() => runApp(MyApp());
 class MyApp extends StatefulWidget {
   @override
