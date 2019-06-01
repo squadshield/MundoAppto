55cfeddc2d1a548b22a8fecf73e2c3b7d3f8c4c4
DBAOscar <adrian_ortiz92>
2019-06-01 10:35:46 -0500

    Imagenes agregadas al listado. V2

--- lib/providers/listado_provider.dart
+++ lib/providers/listado_provider.dart
@@ -1,7 +1,7 @@
 class ListadoProvider {
 List<Map<String, dynamic>> users = [
-  { "nombre": "UTEC", "descripcion": "Universidad de Ingeniería y Tecnología", "ubicacion":"Jr Calle 123", "ranking":"5.0", "imagen":"", "comentario":"Buen lugar para ser el mejor", "lat" : -12.15555617010728, "lng" : -77.02123532010726 },
-  { "nombre": "Estación Aramburú", "descripcion": "Estación del Metropolitano", "ubicacion":"Av. La nueva Esperanza 201", "ranking":"3.9", "imagen":"", "comentario":"Mucha cola para solo un tramo corto. Deberían haber mas expresos!", "lat" : -12.15144617010728, "lng" : -77.02123082010726  },
-  { "nombre": "Restaurante Marita", "descripcion": "Buena sazón!", "ubicacion":"Av. Los Robles 441", "ranking":"4.1", "imagen":"", "comentario":"La señora atiende muy bien","lat" : -12.15234617010728, "lng" : -77.02333082010726  },
+  { "nombre": "UTEC", "descripcion": "Universidad de Ingeniería y Tecnología", "ubicacion":"Jr Calle 123", "ranking":"5.0", "imagen":"https://www.utec.edu.pe/sites/default/files//styles/noticias_slider/public/noticias/web_grande_1.png", "comentario":"Buen lugar para ser el mejor", "lat" : -12.15555617010728, "lng" : -77.02123532010726 },
+  { "nombre": "Estación Aramburú", "descripcion": "Estación del Metropolitano", "ubicacion":"Av. La nueva Esperanza 201", "ranking":"3.9", "imagen":"https://fastly.4sqi.net/img/general/width960/43mI9lDWgx9P51A3QNx0MQMn1MUjNkPsY3NEAqZGB6M.jpg", "comentario":"Mucha cola para solo un tramo corto. Deberían haber mas expresos!", "lat" : -12.15144617010728, "lng" : -77.02123082010726  },
+  { "nombre": "Restaurante Marita", "descripcion": "Buena sazón!", "ubicacion":"Av. Los Robles 441", "ranking":"4.1", "imagen":"https://u.tfstatic.com/restaurant_photos/029/411029/169/612/masterchef-vista-sala-188d9.jpg", "comentario":"La señora atiende muy bien","lat" : -12.15234617010728, "lng" : -77.02333082010726  },
 ];
 }
