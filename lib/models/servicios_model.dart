f47e5b28d0ea76118b5da092b4fe2301162f2c63
DBAOscar <adrian_ortiz92>
2019-06-01 14:09:46 -0500

    Cards realiza match con firebase sobre maps.

--- lib/models/servicios_model.dart
+++ lib/models/servicios_model.dart
@@ -1,58 +1,58 @@
-class Servicios{
-  List<Servicios> items =  new List();
+// class Servicios{
+//   List<Servicios> items =  new List();
 
-  Servicios();
-  Servicios.fromJsonList(List<dynamic> jsonList){
-    if(jsonList =null)return;
-    for (var item in jsonList) {
-      final servicios = new Servicios.fromJsonMap(item);
-      items.add(servicios);   
-    }
-  }
-}
-class Servicios {
+//   Servicios();
+//   Servicios.fromJsonList(List<dynamic> jsonList){
+//     if(jsonList =null)return;
+//     for (var item in jsonList) {
+//       final servicios = new Servicios.fromJsonMap(item);
+//       items.add(servicios);   
+//     }
+//   }
+// }
+// class Servicios {
   
-  String icon;
-  String id;
-  String name;
+//   String icon;
+//   String id;
+//   String name;
 
-  Servicios({
+//   Servicios({
 
-    this.icon,
-    this.id,
-    this.name,
+//     this.icon,
+//     this.id,
+//     this.name,
 
-  });
+//   });
 
-  Servicios.fromJsonMap(Map<String,dynamic> json){
-      icon          = json["icon"];
-      id            =json["id"];
-      name          = json["name"];
-  }
-}
+//   Servicios.fromJsonMap(Map<String,dynamic> json){
+//       icon          = json["icon"];
+//       id            =json["id"];
+//       name          = json["name"];
+//   }
+// }
 
-class Icon {
-  bool icon;
+// class Icon {
+//   bool icon;
 
-  Icon({
-    this.icon,
-  });
-}
+//   Icon({
+//     this.icon,
+//   });
+// }
 
-class Id {
-  bool id;
+// class Id {
+//   bool id;
 
-  Id({
-    this.id,
-  });
-}
+//   Id({
+//     this.id,
+//   });
+// }
 
-class Name {
-  bool name;
+// class Name {
+//   bool name;
 
-  Name({
-    this.name,
-  });
-}
+//   Name({
+//     this.name,
+//   });
+// }
 
-enum Type { RESTAURANT, FOOD, POINT_OF_INTEREST, ESTABLISHMENT, BAR, CAFE, NIGHT_CLUB }
+// enum Type { RESTAURANT, FOOD, POINT_OF_INTEREST, ESTABLISHMENT, BAR, CAFE, NIGHT_CLUB }
--- lib/pages/card_page_dart.dart
+++ lib/pages/card_page_dart.dart
@@ -1,4 +1,4 @@
-import 'dart:typed_data';
+//import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:mundoappto/models/ubicacion_model.dart';
